7812478da538750a2cec1335d20a275159445a7c
liki31 <76156601+liki31@users.noreply.github.com>
2024-03-18 07:25:40 +0000

    File name changed and conversion added

---
+++ 1_Basics/datatypes&Conversion.js
@@ -0,0 +1,9 @@
+Number()
+String()
+Boolean()
+
+// every String can't be converted into Number but every number can be converted into String
+// true <=> 1 & false <=> 0
+// empty string => false & string => true
+// Number("33") = 33
+// Number("33abc") = NaN => Not a Number
