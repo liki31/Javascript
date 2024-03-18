1c24eb4ef5e043425b946904e8ffe4f1eccae0e3
liki31 <76156601+liki31@users.noreply.github.com>
2024-03-18 13:59:07 +0000

    Strings & array

---
+++ 1_Basics/String.js
@@ -0,0 +1,5 @@
+const name = "Likith"
+const repoCount = 50
+
+// `` - back ticks or template literal, ${} - using these two you can inject variables to the display
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`) 
